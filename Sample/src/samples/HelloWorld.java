128de156f77b0bc44bf0b20bb4383f4766badcaa
vikram11102001 <karthikeyavikram1101@gmail.com>
2023-12-12 15:24:02 +0530

    testcommit

---
+++ Sample/src/samples/HelloWorld.java
@@ -0,0 +1,11 @@
+package samples;
+
+public class HelloWorld {
+	
+	//print hello worlds
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("hehur");
+	}
+
+}
